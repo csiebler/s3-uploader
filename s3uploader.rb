f99d5e62211bbafd90ccf88f8b8f41b5ff00f298
Clemens Siebler <clemens.siebler@gmail.com>
2017-02-07 16:30:50 +0100

    Added simple viewing feature

--- s3uploader.rb
+++ s3uploader.rb
@@ -8,6 +8,11 @@ client = Aws::S3::Client.new(region: 'us-east-1', endpoint: ENV['S3_ENDPOINT'],
 s3 = Aws::S3::Resource.new(client: client)
 
 get "/" do
+  @objects = []
+  s3.bucket(S3_BUCKET_NAME).objects.each do |object|
+    @objects << {:key => object.key, :size => object.size}
+  end
+
   haml :index
 end
 
@@ -15,5 +20,7 @@ post "/upload" do
   key = params['file'][:filename]
   #TODO Check if object already exists
   s3.bucket(S3_BUCKET_NAME).object(key).put(body: params['file'][:tempfile].read)
-  haml :index
+  #TODO Forward to error message if upload failed
+
+  redirect to('/')
 end
