d782580969f79b41976083980bd6a6f8e42f75b7
Clemens Siebler <clemens.siebler@gmail.com>
2017-02-08 16:08:37 +0100

    Added auto-reload on scrolling for showing more files

--- public/script.js
+++ public/script.js
@@ -1,18 +1,35 @@
 $(document).ready(function () {
     var marker = '0';
+    var all_loaded = false;
 
-    $("#loadmore").click(function () {
-        if (!$('#loadmore').hasClass('disabled')) {
-            $.get('/load/' + marker, function (data) {
-                if (data) {
-                    $('#files > tbody:last-child').append(data);
-                    marker = $('#files tr:last td:first').html();
-                } else {
-                    $('#loadmore').addClass('disabled');
-                }
-            });
-        };
+    $.get('/load/' + marker, function (data) {
+        if (data) {
+            $('#files > tbody:last-child').append(data);
+            marker = $('#files tr:last td:first').html();
+        }
     });
 
-    $("#loadmore").click();
+    $(window).data('ajaxready', true).scroll(function () {
+        if (all_loaded || $(window).data('ajaxready') == false)
+            return;
+
+        console.log("scroll triggered");
+        if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+            $(window).data('ajaxready', false);
+            marker = $('#files tr:last td:first').html();
+            $.ajax({
+                url: "load/" + marker,
+                success: function (data) {
+                    if (data) {
+                        $('#files > tbody:last-child').append(data);
+                    } else {
+                        $('#loader').fadeOut();
+                        all_loaded = true;
+                    }
+                }
+            });
+            $(window).data('ajaxready', true);
+        }
+
+    });
 });
