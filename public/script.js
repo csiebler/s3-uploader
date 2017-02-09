97a8ebf1f6180ff072de5ec39b6f8feeb552a325
Clemens Siebler <clemens.siebler@gmail.com>
2017-02-09 10:22:47 +0100

    Added support for showing different versions

--- public/script.js
+++ public/script.js
@@ -9,11 +9,31 @@ $(document).ready(function () {
         }
     });
 
+    $.get('/versioning', function (value) {
+        if (value) {
+            $('#versioning').append(value);
+        }
+    });
+
+    $(document).on('click', '.openmodal', function () {
+        var key = $(this).attr('key');
+        $('#versions').empty();
+        $('#versionloader').show();
+        $('#versions-modal').modal('show');
+        $.ajax({
+            url: key + "/versions",
+            success: function (data) {
+                if (data) {
+                    $('#versionloader').fadeOut();
+                    $('#versions').append(data);
+                }
+            }
+        });
+    });
+
     $(window).data('ajaxready', true).scroll(function () {
         if (all_loaded || $(window).data('ajaxready') == false)
             return;
-
-        console.log("scroll triggered");
         if ($(window).scrollTop() == $(document).height() - $(window).height()) {
             $(window).data('ajaxready', false);
             marker = $('#files tr:last td:first').html();
--- s3uploader.rb
+++ s3uploader.rb
@@ -12,28 +12,61 @@ get "/" do
   haml :index
 end
 
+get "/versioning" do
+  s3.get_bucket_versioning({bucket: S3_BUCKET_NAME}).status
+end
+
 get '/load/:marker' do |marker|
     @objects = []
     response = s3.list_objects({bucket: S3_BUCKET_NAME, max_keys: 25, marker: marker})
     response.contents.each do |o|
-      @objects << {key: o.key, size: (o.size / 1048576.0).round(1), 
-                   date: o.last_modified,
-                   id: Base64.strict_encode64(o.key)}
+      @objects << {key: o.key, size: size_in_mb(o.size), 
+                   date: o.last_modified, id: encode(o.key)}
     end
     haml :files
 end
 
 post "/upload" do
-  #TODO Check if object already exists
   #TODO Forward to error message if upload failed
   key = params['file'][:filename]
   s3.put_object({bucket: S3_BUCKET_NAME, key: key, body: params['file'][:tempfile].read})
   redirect to('/')
 end
 
-get "/download/:id" do |id|
-  key = Base64.strict_decode64(id)
+get "/:id/download" do |id|
+  key = decode(id)
   url = signer.presigned_url(:get_object, bucket: S3_BUCKET_NAME, key: key,
                              expires_in: 30, response_content_disposition: 'attachment') 
   redirect to(url)
+end
+
+get "/:id/download/:version" do |id, version|
+  key = decode(id)
+  url = signer.presigned_url(:get_object, bucket: S3_BUCKET_NAME, key: key, version_id: version,
+                             expires_in: 30, response_content_disposition: 'attachment') 
+  redirect to(url)
+end
+
+get "/:id/versions" do |id|
+  key = decode(id)
+  @objects = []
+  response = s3.list_object_versions({bucket: S3_BUCKET_NAME, prefix: key})
+  # TODO Remove isLatest
+  response.versions.each do |o|
+    @objects << {key: o.key, size: size_in_mb(o.size),
+                 date: o.last_modified, version: o.version_id, id: encode(o.key)}
+  end
+  haml :versions
+end
+
+def size_in_mb(value)
+  (value / 1048576.0).round(1)
+end
+
+def encode(value)
+  Base64.strict_encode64(value)
+end
+
+def decode(value)
+  Base64.strict_decode64(value)
 end
