e186595abb427958778756997bbf1dd7e17dbe54
Clemens Siebler <clemens.siebler@gmail.com>
2017-11-23 16:22:43 +0100

    Added error checking if S3 connection is not working

--- s3uploader.rb
+++ s3uploader.rb
@@ -8,7 +8,11 @@ s3 = Aws::S3::Client.new(region: 'us-east-1', endpoint: S3_ENDPOINT, force_path_
 signer = Aws::S3::Presigner.new({client: s3})
 
 get "/" do
-  haml :index
+  if is_s3_connection_working(s3)
+    haml :index
+  else
+    haml :error
+  end
 end
 
 get "/versioning" do
@@ -16,16 +20,16 @@ get "/versioning" do
 end
 
 get '/load/:marker/?' do |marker|
-    marker = decode(marker)
-    @objects = get_reloaded_objects(s3, marker)
-    haml :files
+  marker = decode(marker)
+  @objects = get_reloaded_objects(s3, marker)
+  haml :files
 end
 
 get '/load/:marker/:prefix' do |marker, prefix|
-    marker = decode(marker)
-    prefix = decode(prefix)
-    @objects = get_reloaded_objects(s3, marker, prefix)
-    haml :files
+  marker = decode(marker)
+  prefix = decode(prefix)
+  @objects = get_reloaded_objects(s3, marker, prefix)
+  haml :files
 end
 
 post "/upload" do
@@ -73,10 +77,18 @@ def decode(value)
 end
 
 def get_reloaded_objects(s3, marker, prefix = '')
-    @objects = []
-    response = s3.list_objects({bucket: S3_BUCKET_NAME, max_keys: 100, prefix: prefix, marker: marker})
-    response.contents.each do |o|
-      @objects << {key: o.key, size: size_in_mb(o.size), date: o.last_modified, id: encode(o.key)}
-    end
-    return @objects
+  @objects = []
+  response = s3.list_objects({bucket: S3_BUCKET_NAME, max_keys: 100, prefix: prefix, marker: marker})
+  response.contents.each do |o|
+    @objects << {key: o.key, size: size_in_mb(o.size), date: o.last_modified, id: encode(o.key)}
+  end
+  return @objects
+end
+
+def is_s3_connection_working(s3)
+  begin
+    return s3.list_buckets.buckets.any? {|b| b[:name] == S3_BUCKET_NAME}
+  rescue
+    false
+  end
 end
